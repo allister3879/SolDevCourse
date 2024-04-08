9be7d1c8d04eaa7090366455957cafb756de9042
allister3879 <129583359+allister3879@users.noreply.github.com>
2024-04-09 02:17:12 +0500

    module 1 lab 3

---
+++ generate-keypair/transfer.ts
@@ -0,0 +1,23 @@
+const {Connection, Transaction,SystemProgram, sendAndConfirmTransaction, PublicKey} = require('@solana/web3.js');
+require('dotenv').config();
+
+import { getKeypairFromEnvironment } from "@solana-developers/helpers";
+
+const suppliedToPubkey = process.argv[2] || null;
+  
+if (!suppliedToPubkey) {
+    console.log(`Please provide a public key to send to`);
+    process.exit(1);
+}
+  
+const senderKeypair = getKeypairFromEnvironment("SECRET_KEY");
+  
+console.log(`suppliedToPubkey: ${suppliedToPubkey}`);
+  
+const toPubkey = new PublicKey(suppliedToPubkey);
+  
+const connection = new Connection("https://api.devnet.solana.com", "confirmed");
+  
+console.log(
+    `✅ Loaded our own keypair, the destination public key, and connected to Solana`
+);
